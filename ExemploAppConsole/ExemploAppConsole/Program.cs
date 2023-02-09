7ddd366d4efc7d32176ebe6486c2a3dcc51cb66e
Nigraum <yan.corintima@hotmail.com>
2023-02-09 01:55:10 -0300

    Create ExemploAppConsole project

---
+++ ExemploAppConsole/ExemploAppConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Yan");
+Console.WriteLine("yan.corintima@hotmail.com");
+Console.WriteLine("12312-31231");
+Console.ReadLine();
