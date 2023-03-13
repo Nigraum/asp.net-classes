3dbd41954683cdba4e6cdb9aa18ed90e57521b2b
Nigraum <yan.corintima@hotmail.com>
2023-03-13 19:13:37 -0300

    Finishing to learn primitive types

--- TiposPrimitivos/TiposPrimitivos/Program.cs
+++ TiposPrimitivos/TiposPrimitivos/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Tipos Primitivos");
+
+            string nome = "José";
+            int numero = 20;
+            long numeroGrande = 120000000;
+            byte item = 20;
+
+            double comissao = 32.45213415521521;
+            decimal valor = 1000.56m; //convertendo para decimal com a letra M
+
+            DateTime data = DateTime.Now;
+            DateTime exemplo = new DateTime(2015, 12, 31);
+            Console.WriteLine(data);
+            Console.WriteLine(exemplo);
+            
+
         }
     }
 }
