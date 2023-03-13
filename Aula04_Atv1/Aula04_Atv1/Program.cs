ee954e13d9865af265ce5b75b52e73d2d9117458
Nigraum <yan.corintima@hotmail.com>
2023-03-13 16:56:49 -0300

    Finishing activity from aula4

---
+++ Aula04_Atv1/Aula04_Atv1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula04_Atv1
+{
+    //Classe
+    class Program
+    {
+        //Método principal
+        static void Main(string[] args)
+        {
+            //Somar dois números
+            int a, b, result;
+
+            Console.Write("Digite o número 1: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o número 2: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            result = a + b;
+            Console.WriteLine("A soma de {0} e {1} é {2}", a, b, result);
+            Console.ReadLine();
+        }
+    }
+}
