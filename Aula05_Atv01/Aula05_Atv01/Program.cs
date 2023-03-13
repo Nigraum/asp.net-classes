6ae11dac1096ce1914512f531aa38e452c1f326d
Nigraum <yan.corintima@hotmail.com>
2023-03-13 19:23:00 -0300

    Finishing exercise from aula05

--- Aula05_Atv01/Aula05_Atv01/Program.cs
+++ Aula05_Atv01/Aula05_Atv01/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digite o valor do produto: ");
+decimal valor = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite a quantidade: ");
+int qtd = Convert.ToInt32(Console.ReadLine());
+
+decimal resultado = qtd * valor;
+Console.WriteLine("O valor total é: {0:c}", resultado); //a letra c Formata para o formato da moeda
