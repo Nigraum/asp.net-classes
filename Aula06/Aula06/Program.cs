f7753541872dc0f5379802ed632700181e92edac
Nigraum <yan.corintima@hotmail.com>
2023-03-14 18:08:45 -0300

    Create aula06 project

---
+++ Aula06/Aula06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite um número entre 1 e 10: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n > 10)
+{
+    Console.Write("O número não está entre 1 e 10");
+}
+else
+{
+    Console.Write("Obrigado.");
+}
