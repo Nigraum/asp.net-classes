fc04abd766fd1aee13d0bf51e4d13b2542a157f2
Nigraum <yan.corintima@hotmail.com>
2023-03-14 18:50:59 -0300

    Finishing exercise from aula06

--- Aula06Atv01/Aula06Atv01/Program.cs
+++ Aula06Atv01/Aula06Atv01/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Formas de pagamento
+//Se o valor da compra for maior que 1000.00 reais, pode parcelar em 3 vezes
+//Se não for, apenas a vista
+
+decimal valorCompra;
+int numeroParcelas = 3;
+decimal valorParcela;
+decimal ValorMinimoParcela = 1000.01m;
+
+Console.Write("Digite o valor da compra: ");
+valorCompra = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Valor a vista: {0:c}", valorCompra);
+
+if (valorCompra >= ValorMinimoParcela)
+{
+    valorParcela = valorCompra / numeroParcelas;
+    Console.WriteLine("Pode ser parcelado em {0} vezes. Valor da parcela: {1:c}", numeroParcelas, valorParcela);
+}
