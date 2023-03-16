2d2d343ca2ff6818672f9f05a1033ea3a2d76e22
Nigraum <yan.corintima@hotmail.com>
2023-03-16 20:00:30 -0300

    Finishing exercise 2 from aula08

--- Aula08Atv02/Aula08Atv02/Form1.Designer.cs
+++ Aula08Atv02/Aula08Atv02/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            tabuadaTextBox = new TextBox();
+            okButton = new Button();
+            tabuadaListBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(34, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Digite a tabuada";
+            // 
+            // tabuadaTextBox
+            // 
+            tabuadaTextBox.Location = new Point(133, 19);
+            tabuadaTextBox.Name = "tabuadaTextBox";
+            tabuadaTextBox.Size = new Size(94, 23);
+            tabuadaTextBox.TabIndex = 1;
+            // 
+            // okButton
+            // 
+            okButton.Location = new Point(233, 19);
+            okButton.Name = "okButton";
+            okButton.Size = new Size(75, 23);
+            okButton.TabIndex = 2;
+            okButton.Text = "Ok";
+            okButton.UseVisualStyleBackColor = true;
+            okButton.Click += okButton_Click;
+            // 
+            // tabuadaListBox1
+            // 
+            tabuadaListBox1.FormattingEnabled = true;
+            tabuadaListBox1.ItemHeight = 15;
+            tabuadaListBox1.Location = new Point(34, 82);
+            tabuadaListBox1.Name = "tabuadaListBox1";
+            tabuadaListBox1.Size = new Size(360, 199);
+            tabuadaListBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(489, 311);
+            Controls.Add(tabuadaListBox1);
+            Controls.Add(okButton);
+            Controls.Add(tabuadaTextBox);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox tabuadaTextBox;
+        private Button okButton;
+        private ListBox tabuadaListBox1;
     }
 }
--- Aula08Atv02/Aula08Atv02/Form1.cs
+++ Aula08Atv02/Aula08Atv02/Form1.cs
@@ -6,5 +6,17 @@ namespace Aula08Atv02
         {
             InitializeComponent();
         }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            tabuadaListBox1.Items.Clear();
+
+            int tabuada = Convert.ToInt32(tabuadaTextBox.Text);
+            for (int i = 1; i <= 10; i++)
+            {
+                string linha = string.Format("{0} x {1} = {2}", tabuada, i, i * tabuada);
+                tabuadaListBox1.Items.Add(linha);
+            }
+        }
     }
 }
