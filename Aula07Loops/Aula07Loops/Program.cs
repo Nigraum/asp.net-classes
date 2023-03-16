07ef38491c431b1c2e7d0756b73c4e90ad80c2a5
Nigraum <yan.corintima@hotmail.com>
2023-03-16 18:23:29 -0300

    Finishing aula07

--- Aula07Loops/Aula07Loops/Program.cs
+++ Aula07Loops/Aula07Loops/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string senha = "123";
+string senhaInformada = string.Empty;
+
+do
+{
+    Console.Write("Digite a senha: ");
+    senhaInformada = Console.ReadLine();
+
+    if(senhaInformada != senha)
+    {
+        Console.WriteLine("Senha incorreta.");
+    }
+
+} while (senhaInformada != senha);
+
+/*int x = 1;
+
+while (x <= 10)
+{
+    Console.WriteLine(x);
+    x++;   
+}
+*/
+
+/*string senha = "123";
+string senhaInformada = string.Empty;
+
+while (senha != senhaInformada)
+{
+    Console.Write("Digite a senha: ");
+    senhaInformada = Console.ReadLine();
+}
+*/
+
+/*for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+*/
