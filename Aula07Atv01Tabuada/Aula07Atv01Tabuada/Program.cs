91b02ae0061585ab663f97c770177881eacaeb83
Nigraum <yan.corintima@hotmail.com>
2023-03-16 18:36:39 -0300

    Create activity from aula07

---
+++ Aula07Atv01Tabuada/Aula07Atv01Tabuada/Program.cs
@@ -0,0 +1,8 @@
+﻿int tabuada;
+Console.Write("Qual tabuada você deseja? ");
+tabuada = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("{0} x {1} = {2}", tabuada, i, i*tabuada );
+}
