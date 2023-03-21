e85a63b489e9fd052c83a569971c632633cbfcc7
Nigraum <yan.corintima@hotmail.com>
2023-03-21 18:27:10 -0300

    Starting to learn about arrays in C#

---
+++ Aula09Arrays/Aula09Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿//arrays 
+
+int[] numeros = new int[6];
+numeros[0] = 40;
+numeros[1] = 5;
+numeros[2] = 43;
+numeros[3] = 17;
+numeros[4] = 52;
+numeros[5] = 10;
+
+foreach  (int n in numeros)
+{
+    Console.WriteLine(n);
+}
