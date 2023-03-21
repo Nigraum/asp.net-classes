6c9150dc135a8ff74ba9392f9f75b82e781a2662
Nigraum <yan.corintima@hotmail.com>
2023-03-21 18:47:30 -0300

    Finishing class about arrays

--- Aula09Arrays/Aula09Arrays/Program.cs
+++ Aula09Arrays/Aula09Arrays/Program.cs
@@ -8,7 +8,28 @@ numeros[3] = 17;
 numeros[4] = 52;
 numeros[5] = 10;
 
+//Percorrendo
 foreach  (int n in numeros)
+{
+    Console.WriteLine(n);
+}
+
+//Ordenar
+Array.Sort(numeros);
+
+//Pesquisar
+int indice = Array.IndexOf(numeros, 40);
+if(indice == -1)
+{
+    Console.WriteLine("17 não encontrado");
+} else
+{
+    Console.WriteLine("Econtrado:{0} na posição {1}", numeros[indice], indice);
+}
+
+Console.WriteLine("-----");
+
+foreach (int n in numeros)
 {
     Console.WriteLine(n);
 }
